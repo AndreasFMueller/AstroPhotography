3fccfcc134a5d338587f84aa1835026cc30f3c02
Andreas Mueller <afm@othello.ch>
2013-04-09 23:34:30 +0200

    add microtouchtests.cpp

---
+++ control/lib/test/microtouchtests.cpp
@@ -0,0 +1,16 @@
+/*
+ * microtouchtests.cpp
+ *
+ * (c) 2012 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ * $Id$
+ */
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int	main(int argc, char *argv[]) {
+	CppUnit::TextUi::TestRunner	runner;
+	CppUnit::TestFactoryRegistry	&registry = CppUnit::TestFactoryRegistry::getRegistry();
+	runner.addTest(registry.makeTest());
+	bool	wasSuccessful = runner.run("", false);
+	return (wasSuccessful) ? 0 : 1;
+}
