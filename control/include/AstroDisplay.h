831766b251e6701336aac0679c1fed8d06817c4d
Andreas Mueller <afm@othello.ch>
2013-07-25 20:13:13 +0200

    add new header file for display image functions

---
+++ control/include/AstroDisplay.h
@@ -0,0 +1,18 @@
+/*
+ * AstroDisplay.h -- conversion functions to produce displayable versions
+ *                   of astro images
+ *
+ * (c) 2013 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#ifndef _AstroDisplay_h
+#define _AstroDisplay_h
+
+#include <AstroImage.h>
+
+namespace astro {
+namespace image {
+
+} // namespace image
+} // namespace astro
+
+#endif /* _AstroDisplay_h */
