9696016891c0542a7fb1b08fb05ad4f568b36c54
Andreas Müller <andreas.mueller@othello.ch>
2013-08-13 15:43:14 +0200

    add NameService usage

--- control/client/astroclient.cpp
+++ control/client/astroclient.cpp
@@ -12,6 +12,12 @@
 namespace astro {
 
 int	main(int argc, char *argv[]) {
+	// get an orb reference, also removes the ORB arguments from
+	// the command line
+	CORBA::ORB_ptr	orb = CORBA::ORB_init(argc, argv, "omniORB4");
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "got ORB");
+
+	// parse the command line
 	int	c;
 	while (EOF != (c = getopt(argc, argv, "d")))
 		switch (c) {
@@ -20,36 +26,60 @@ int	main(int argc, char *argv[]) {
 			break;
 		}
 
-	// next argument must be the IOR
-	if (optind >= argc) {
-		std::cerr << "no IOR specified" << std::endl;
+	// get a reference to the naming service
+	CORBA::Object_var	initServ;
+	try {
+		initServ = orb->resolve_initial_references("NameService");
+	} catch (CORBA::ORB::InvalidName& x) {
+		std::cerr << "no naming service" << std::endl;
+		throw std::runtime_error("no naming service");
+	}
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "got naming service");
+
+	CosNaming::NamingContext_var	rootContext;
+	rootContext = CosNaming::NamingContext::_narrow(initServ);
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "got naming service root context");
+
+	// Next we want to get a reference to the Modules object
+	CORBA::Object_var	obj;
+
+	// prepare the path for object lookup in the root context
+	CosNaming::Name	name;
+	name.length(2);
+	name[0].id = (const char *)"Astro";
+	name[0].kind = (const char *)"context";
+	name[1].id = (const char *)"Modules";
+	name[1].kind = (const char *)"object";
+	try {
+		obj = rootContext->resolve(name);
+	} catch (CosNaming::NamingContext::NotFound& ex) {
+		std::cerr << "context not found" << std::endl;
+		return EXIT_FAILURE;
+	} catch (CORBA::TRANSIENT& ex) {
+		return EXIT_FAILURE;
+	} catch (CORBA::SystemException& ex) {
 		return EXIT_FAILURE;
 	}
-	char	*ior = argv[optind];
-	debug(LOG_DEBUG, DEBUG_LOG, 0, "working with %s", ior);
-
-	// get an orb reference
-	CORBA::ORB_ptr	orb = CORBA::ORB_init(argc, argv, "omniORB4");
-
-	// getting an object reference
-	CORBA::Object_var	obj = orb->string_to_object(ior);
 
 	// get a reference to the modules interface
 	Astro::Modules_var	modules = Astro::Modules::_narrow(obj);
 	if (CORBA::is_nil(modules)) {
 		throw std::runtime_error("nil object reference");
 	}
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "got a reference to a Modules object");
 
 	// convert the object into a Modules object
 	std::cout << "number of modules: " << modules->numberOfModules()
 		<< std::endl;
 
-	// get the list of all modules
-	Astro::Modules::ModuleNameSequence_var	names = modules->getModuleNames();
+	// get the list of all modules, and display it
+	Astro::Modules::ModuleNameSequence_var	names
+		= modules->getModuleNames();
 	for (int i = 0; i < names->length(); i++) {
 		std::cout << "module " << i << ": " << names[i] << std::endl;
 	}
 
+	// that's it, we are done
 	exit(EXIT_SUCCESS);
 }
 
--- control/server/astrod.cpp
+++ control/server/astrod.cpp
@@ -23,9 +23,11 @@ int	main(int argc, char *argv[]) {
 		}
 
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "astrod starting up");
+
 	// initialize CORBA
 	CORBA::ORB_var	orb = CORBA::ORB_init(argc, argv);
-	CORBA::Object_var	obj = orb->resolve_initial_references("RootPOA");
+	CORBA::Object_var	obj
+		= orb->resolve_initial_references("RootPOA");
 	PortableServer::POA_var	poa = PortableServer::POA::_narrow(obj);
 
 	// create the servant and register it with the ORB
@@ -39,6 +41,64 @@ int	main(int argc, char *argv[]) {
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "IOR: %s", (char *)sior);
 
 	modules->_remove_ref();
+
+	// get a reference to the root context to bind the module reference to
+	// a name for the client
+	CosNaming::NamingContext_var	rootContext;
+	try {
+		CORBA::Object_var	obj;
+		obj = orb->resolve_initial_references("NameService");
+		rootContext = CosNaming::NamingContext::_narrow(obj);
+		if (CORBA::is_nil(rootContext)) {
+			std::cerr << "failed to narrow root naming context"
+				<< std::endl;
+			return EXIT_FAILURE;
+		}
+	} catch (CORBA::NO_RESOURCES&) {
+		std::cerr << "omniORB is not configured" << std::endl;
+		return EXIT_FAILURE;
+	} catch (CORBA::ORB::InvalidName&) {
+		std::cerr << "Service required is invalid" << std::endl;
+		return EXIT_FAILURE;
+	}
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "naming service initialized");
+
+	// create a context for Astro
+	try {
+		// prepare the name for the context
+		CosNaming::Name	contextName;
+		contextName.length(1);
+		contextName[0].id = (const char *)"Astro";
+		contextName[0].kind = (const char *)"context";
+
+		// bind the context
+		CosNaming::NamingContext_var	context;
+		try {
+			context = rootContext->bind_new_context(contextName);
+		} catch (CosNaming::NamingContext::AlreadyBound& ex) {
+			CORBA::Object_var	obj
+				= rootContext->resolve(contextName);
+			context = CosNaming::NamingContext::_narrow(obj);
+			if (CORBA::is_nil(context)) {
+				std::cerr << "failed to narrow naming context"
+					<< std::endl;
+			}
+		}
+
+		// register the name
+		CosNaming::Name	objectName;
+		objectName.length(1);
+		objectName[0].id = (const char *)"Modules";
+		objectName[0].kind = (const char *)"object";
+		try {
+			context->bind(objectName, obj);
+		} catch (CosNaming::NamingContext::AlreadyBound& ex) {
+			context->rebind(objectName, obj);
+		}
+	} catch (CORBA::TRANSIENT& ex) {
+	} catch (CORBA::SystemException& ex) {
+	}
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "object now bound");
 
 	PortableServer::POAManager_var	pman = poa->the_POAManager();
 	pman->activate();
