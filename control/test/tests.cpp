7be55db1e271d9e1d2ff956356c40adf65b3a91c
Andreas Mueller <afm@othello.ch>
2013-08-19 19:08:21 +0200

    add missing include

--- control/test/tests.cpp
+++ control/test/tests.cpp
@@ -7,6 +7,7 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 #include <AstroDebug.h>
+#include <includes.h>
 
 int	main(int argc, char *argv[]) {
 	int	c;
