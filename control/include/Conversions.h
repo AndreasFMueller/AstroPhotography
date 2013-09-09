f6329b89b6626d8354a68e8dc9d74b69b3db73a0
Andreas Müller <andreas.mueller@othello.ch>
2013-09-09 15:15:42 +0200

    fix corba includes

--- control/include/Conversions.h
+++ control/include/Conversions.h
@@ -6,7 +6,7 @@
 #ifndef _Conversions_h
 #define _Conversions_h
 
-#include <astro.hh>
+#include <module.hh>
 #include <AstroTypes.h>
 #include <AstroLocator.h>
 #include <AstroCamera.h>
--- control/include/OrbSingleton.h
+++ control/include/OrbSingleton.h
@@ -6,7 +6,7 @@
 #ifndef _OrbSingleton_h
 #define _OrbSingleton_h
 
-#include <astro.hh>
+#include <module.hh>
 #include <string>
 
 namespace Astro {
