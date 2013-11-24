9da8205d9361173f8811b7b2211ab60848fb3038
Andreas Mueller <afm@othello.ch>
2013-11-24 18:45:11 +0100

    add missing files

---
+++ control/guider/guidercommand.cpp
@@ -0,0 +1,184 @@
+/*
+ * guidercommand.cpp -- guider command implementation
+ *
+ * (c) 2103 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#include <guidercommand.h>
+#include <AstroDebug.h>
+//#include <guiders.h>
+#include <iostream>
+
+namespace astro {
+namespace cli {
+
+std::ostream&	operator<<(std::ostream& out,
+			const Astro::ImageRectangle& rectangle) {
+	out << rectangle.size.width << "x" << rectangle.size.height << "@("
+		<< rectangle.origin.x << "," << rectangle.origin.y << ")";
+	return out;
+}
+
+std::ostream&	operator<<(std::ostream& out, const Astro::BinningMode& mode) {
+	out << mode.x << "x" << mode.y;
+	return out;
+}
+
+std::ostream&	operator<<(std::ostream& out, const Astro::Exposure& exposure) {
+	out << "exposure time:   " << exposure.exposuretime << std::endl;
+	out << "rectangle:       " << exposure.frame << std::endl;
+	out << "gain:            " << exposure.gain << std::endl;
+	out << "limit:           " << exposure.limit << std::endl;
+	out << "shutter:         "
+		<< ((exposure.shutter == Astro::SHUTTER_CLOSED)
+			? "close" : "open")
+		<< std::endl;
+	out << "binning mode:    " << exposure.mode << std::endl;
+	return out;
+}
+
+std::ostream&	operator<<(std::ostream& out, const Astro::Point& star) {
+	out << "point:           (" << star.x << "," << star.y << ")" << std::endl;
+	return out;
+}
+
+std::ostream&	operator<<(std::ostream& out, GuiderWrapper& guider) {
+	out << guider->getExposure();
+	out << guider->getStar();
+	return out;
+}
+
+void	guidercommand::exposure(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	std::cout << guider->getExposure();
+}
+
+void	guidercommand::info(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	std::cout << guider;
+}
+
+void	guidercommand::exposuretime(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	if (arguments.size() < 3) {
+		throw command_error("exposure time missing");
+	}
+	Astro::Exposure	exposure = guider->getExposure();
+	exposure.exposuretime = stof(arguments[2]);
+	guider->setExposure(exposure);
+}
+
+void	guidercommand::binning(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	if (arguments.size() < 4) {
+		throw command_error("binning mode missing");
+	}
+	Astro::Exposure	exposure = guider->getExposure();
+	exposure.mode.x = stoi(arguments[2]);
+	exposure.mode.y = stoi(arguments[3]);
+	guider->setExposure(exposure);
+}
+
+void	guidercommand::offset(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	if (arguments.size() < 4) {
+		throw command_error("viewport window missing");
+	}
+	Astro::Exposure	exposure = guider->getExposure();
+	exposure.frame.origin.x = stoi(arguments[2]);
+	exposure.frame.origin.y = stoi(arguments[3]);
+	guider->setExposure(exposure);
+}
+
+void	guidercommand::size(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	if (arguments.size() < 4) {
+		throw command_error("viewport window missing");
+	}
+	Astro::Exposure	exposure = guider->getExposure();
+	exposure.frame.size.width = stoi(arguments[2]);
+	exposure.frame.size.height = stoi(arguments[3]);
+	guider->setExposure(exposure);
+}
+
+void	guidercommand::star(GuiderWrapper& guider,
+		const std::vector<std::string>& arguments) {
+	if (arguments.size() < 4) {
+		throw command_error("star coordinates missing");
+	}
+	Astro::Point	point;
+	point.x = stoi(arguments[2]);
+	point.y = stoi(arguments[3]);
+	guider->setStar(point);
+}
+
+void	guidercommand::operator()(const std::string& command,
+		const std::vector<std::string>& arguments) {
+	if (arguments.size() < 2) {
+		throw command_error("guider command requires more "
+			"arguments");
+	}
+	std::string	guiderid = arguments[0];
+	std::string	subcommand = arguments[1];
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "guiderid: %s", guiderid.c_str());
+
+	Guiders	guiders;
+	GuiderWrapper	guider = guiders.byname(guiderid);
+
+	if (subcommand == "info") {
+		info(guider, arguments);
+		return;
+	}
+
+	if (subcommand == "exposure") {
+		exposure(guider, arguments);
+		return;
+	}
+
+	if (subcommand == "exposuretime") {
+		exposuretime(guider, arguments);
+		return;
+	}
+
+	if (subcommand == "binning") {
+		binning(guider, arguments);
+		return;
+	}
+
+	if (subcommand == "size") {
+		size(guider, arguments);
+		return;
+	}
+
+	if (subcommand == "offset") {
+		offset(guider, arguments);
+		return;
+	}
+
+	if (subcommand == "star") {
+		star(guider, arguments);
+		return;
+	}
+}
+
+std::string	guidercommand::summary() const {
+	return std::string("create and retrieve guiders");
+}
+
+std::string	guidercommand::help() const {
+	return std::string(
+	"SYNOPSIS\n"
+	"\n"
+	"\tguider <guider> exposure\n"
+	"\tguider <guider> info\n"
+	"\tguider <guider> binning <bin_x> <bin_y>\n"
+	"\tguider <guider> size <x> <y>\n"
+	"\tguider <guider> offset <width> <height>\n"
+	"\tguider <guider> star <x> <y>\n"
+	"\n"
+	"DESCRIPTION\n"
+	"\n"
+	);
+}
+
+} // namespace cli
+} // namespace astro
---
+++ control/guider/guidercommand.h
@@ -0,0 +1,45 @@
+/*
+ * guidercommand.h -- access to the guider
+ *
+ * (c) 2013 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#ifndef _guidercommand_h
+#define _guidercommand_h
+
+#include <clicommand.h>
+#include <Guiders.h>
+
+namespace astro {
+namespace cli {
+
+class guidercommand : public clicommand {
+	void	info(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+	void	exposure(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+
+	void	exposuretime(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+	void	binning(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+	void	size(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+	void	offset(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+
+	void	star(GuiderWrapper& guider,
+			const std::vector<std::string>& arguments);
+public:
+	guidercommand(commandfactory& factory)
+		: clicommand(factory, "guider") { }
+	~guidercommand() { }
+	virtual void	operator()(const std::string& command,
+			const std::vector<std::string>& arguments);
+	virtual std::string	summary() const;
+	virtual std::string	help() const;
+};
+
+} // namespace cli
+} // namespace astro
+
+#endif /* _guidercommand_h */
