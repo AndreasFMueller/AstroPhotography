d7e82f62956ce63d69e5d68f64319cce619ca572
Andreas Mueller <afm@othello.ch>
2014-01-04 11:32:08 +0100

    minor fixes

--- control/tools/convertimage.sh
+++ control/tools/convertimage.sh
@@ -10,8 +10,7 @@ jpegimage=`echo ${fitsimage} | sed -e 's/-.*.fits/.jpeg/'`
 tmpfile=/tmp/t$$.jpeg
 trap "rm -f ${tmpfile}" 0 1 2 3 15
 
-#convert ${fitsimage} -rotate 90 -contrast-stretch 20000x65535 ${tmpfile}
-convert ${fitsimage} -rotate 90 -level 22600x30000 ${tmpfile}
+convert ${fitsimage} -rotate 90 -contrast-stretch 25%x4% ${tmpfile}
 
 convert -size 1040x1040 xc:none -fill ${tmpfile} -draw "circle 520,520 1,520" \
 	${jpegimage}
