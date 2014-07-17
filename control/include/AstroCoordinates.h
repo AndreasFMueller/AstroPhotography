c768884b735a234d4e5207129c118270d5c1c98e
Andreas Mueller <afm@othello.ch>
2014-07-17 07:26:36 +0200

    add arithmetic with spherical coordinates

--- control/include/AstroCoordinates.h
+++ control/include/AstroCoordinates.h
@@ -6,12 +6,21 @@
 #ifndef _AstroCoordinates_h
 #define _AstroCoordinates_h
 
+#include <math.h>
+
 namespace astro {
 
+/**
+ * \brief Angle abstraction
+ *
+ * ANgles are measured in different units of measure, and this class takes
+ * care of these units by providing accessor functions that perform the
+ * conversions. Internally, the angle is always kept in radians.
+ */
 class Angle {
 	double	_angle; // in radians
 protected:
-	virtual void	reduce();
+	virtual void	reduce(double base = 0);
 public:
 	Angle(double angle = 0);
 	double	degrees() const;
@@ -23,24 +32,96 @@ public:
 	Angle	operator+(const Angle& other) const;
 	Angle	operator-(const Angle& other) const;
 	Angle	operator*(const double& other) const;
+	Angle	reduced(const double base = 0);
+static double	hours_to_radians(const double h);
+static double	degrees_to_radians(const double d);
+static double	radians_to_hours(const double r);
+static double	radians_to_degrees(const double r);
 };
 
-class RaDec {
-	Angle	_ra;
-	Angle	_dec;
+double	cos(const Angle& a);
+double	sin(const Angle& a);
+double	tan(const Angle& a);
+double	cot(const Angle& a);
+double	sec(const Angle& a);
+double	csc(const Angle& a);
+
+class TwoAngles {
+	Angle	_a1;
+	Angle	_a2;
 public:
-	RaDec(const Angle& ra, const Angle& dec) : _ra(ra), _dec(dec) { }
-	const Angle&	ra() const { return _ra; }
-	const Angle&	dec() const { return _dec; }
+	TwoAngles() { }
+	TwoAngles(const Angle& a1, const Angle& a2) : _a1(a1), _a2(a2) { }
+	const Angle&	a1() const { return _a1; }
+	Angle&	a1() { return _a1; }
+	const Angle&	a2() const { return _a2; }
+	Angle&	a2() { return _a2; }
+	const Angle&	operator[](int i) const;
+	Angle&	operator[](int i);
 };
 
-class	AzmAlt {
-	Angle	_azm;
-	Angle	_alt;
+class LongLat;
+class RaDec;
+
+class SphericalCoordinates : public TwoAngles {
 public:
-	AzmAlt(const Angle& azm, const Angle& alt) : _azm(azm), _alt(alt) { }
-	const Angle&	azm() const { return _azm; }
-	const Angle&	alt() const { return _alt; }
+	SphericalCoordinates() { }
+	SphericalCoordinates(const Angle& phi, const Angle& theta)
+		: TwoAngles(phi, theta) { }
+	SphericalCoordinates(const LongLat& longlat);
+	SphericalCoordinates(const RaDec& radec);
+	const Angle&	phi() const { return a1(); }
+	Angle&	phi() { return a1(); }
+	const Angle&	theta() const { return a2(); }
+	Angle&	theta() { return a2(); }
+};
+
+class	UnitVector {
+	double	_x[3];
+public:
+	UnitVector(const SphericalCoordinates& spherical);
+	double	operator*(const UnitVector& other) const;
+	double	x() const { return _x[0]; }
+	double	y() const { return _x[1]; }
+	double	z() const { return _x[2]; }
+	Angle	operator-(const UnitVector& other) const;
+};
+
+Angle	operator-(const SphericalCoordinates& s1, const SphericalCoordinates& s2);
+
+class RaDec : public TwoAngles {
+public:
+	RaDec() { }
+	RaDec(const Angle& ra, const Angle& dec) : TwoAngles(ra, dec) { }
+	RaDec(const SphericalCoordinates& spherical)
+		: TwoAngles(spherical.phi(), Angle(M_PI / 2) - spherical.theta()) { }
+	const Angle&	ra() const { return a1(); }
+	Angle&	ra() { return a1(); }
+	const Angle&	dec() const { return a2(); }
+	Angle&	dec() { return a2(); }
+};
+
+class	AzmAlt : public TwoAngles {
+public:
+	AzmAlt() { }
+	AzmAlt(const Angle& azm, const Angle& alt) : TwoAngles(azm, alt) { }
+	const Angle&	azm() const { return a1(); }
+	Angle&	azm() { return a1(); }
+	const Angle&	alt() const { return a2(); }
+	Angle&	alt() { return a2(); }
+};
+
+class	LongLat : public TwoAngles {
+public:
+	LongLat() { }
+	LongLat(const Angle& longitude, const Angle& latitude)
+		: TwoAngles(longitude, latitude) { }
+	LongLat(const SphericalCoordinates& spherical)
+		: TwoAngles(spherical.phi(), Angle(M_PI / 2) - spherical.theta()) { }
+	const Angle&	longitude() const { return a1(); }
+	Angle&	longitude() { return a1(); }
+	const Angle&	latitude() const { return a2(); }
+	Angle&	latitude() { return a2(); }
 };
 
 } // namespace astro
--- control/lib/Coordinates.cpp
+++ control/lib/Coordinates.cpp
@@ -5,16 +5,37 @@
  */
 #include <AstroCoordinates.h>
 #include <cmath>
+#include <stdexcept>
 
 namespace astro {
 
-void	Angle::reduce() {
-	while (_angle > 2 * M_PI) {
-		_angle -= 2 * M_PI;
-	}
-	while (_angle < 0) {
-		_angle += 2 * M_PI;
-	}
+static double	angle_reduction(const double a, const double base) {
+	double	ab = a - base;
+	return base + ab - 2 * M_PI * floor(ab / (2 * M_PI));
+}
+
+void	Angle::reduce(double base) {
+	_angle = angle_reduction(_angle, base);
+}
+
+Angle	Angle::reduced(const double base) {
+	return Angle(angle_reduction(_angle, base));
+}
+
+double	Angle::degrees_to_radians(const double d) {
+	return M_PI * d / 180.;
+}
+
+double	Angle::hours_to_radians(const double h) {
+	return M_PI * h / 12.;
+}
+
+double	Angle::radians_to_hours(const double r) {
+	return 12. * r / M_PI;
+}
+
+double	Angle::radians_to_degrees(const double r) {
+	return 180. * r / M_PI;
 }
 
 Angle::Angle(double angle) : _angle(angle) {
@@ -22,19 +43,19 @@ Angle::Angle(double angle) : _angle(angle) {
 }
 
 double	Angle::degrees() const {
-	return 180 * _angle / M_PI;
+	return radians_to_degrees(_angle);
 }
 
 void	Angle::degrees(double degrees) {
-	_angle = M_PI * degrees / 180;
+	_angle = degrees_to_radians(degrees);
 }
 
 double	Angle::hours() const {
-	return 12 * _angle / M_PI;
+	return radians_to_hours(_angle);
 }
 
 void	Angle::hours(double hours) {
-	_angle = M_PI * hours / 12;
+	_angle = radians_to_hours(hours);
 }
 
 Angle	Angle::operator+(const Angle& other) const {
@@ -47,6 +68,56 @@ Angle	Angle::operator-(const Angle& other) const {
 
 Angle	Angle::operator*(const double& other) const {
 	return Angle(_angle * other);
+}
+
+double	cos(const Angle& a) { return ::cos(a.radians()); }
+double	sin(const Angle& a) { return ::sin(a.radians()); }
+double	tan(const Angle& a) { return ::tan(a.radians()); }
+double	cot(const Angle& a) { return 1. / tan(a); }
+double	sec(const Angle& a) { return 1 / cos(a); }
+double	csc(const Angle& a) { return 1 / sin(a); }
+
+Angle&	TwoAngles::operator[](int i) {
+	switch (i) {
+	case 0:	return a1();
+	case 1: return a2();
+	}
+	throw std::range_error("angle index out of range");
+}
+
+const Angle&	TwoAngles::operator[](int i) const {
+	switch (i) {
+	case 0:	return a1();
+	case 1: return a2();
+	}
+	throw std::range_error("angle index out of range");
+}
+
+SphericalCoordinates::SphericalCoordinates(const LongLat& longlat)
+	: TwoAngles(longlat.longitude(), Angle(M_PI / 2) - longlat.latitude()) {
+}
+
+SphericalCoordinates::SphericalCoordinates(const RaDec& radec)
+	: TwoAngles(radec.ra(), Angle(M_PI / 2) - radec.dec()) {
+}
+
+UnitVector::UnitVector(const SphericalCoordinates& spherical) {
+	_x[2] = cos(spherical.theta());
+	double	r = sin(spherical.theta());
+	_x[0] = r * cos(spherical.phi());
+	_x[1] = r * sin(spherical.phi());
+}
+
+double	UnitVector::operator*(const UnitVector& other) const {
+	return _x[0] * other._x[0] + _x[1] * other._x[1] + _x[2] * other._x[2];
+}
+
+Angle	UnitVector::operator-(const UnitVector& other) const {
+	return Angle(acos(*this * other));
+}
+
+Angle	operator-(const SphericalCoordinates& s1, const SphericalCoordinates& s2) {
+	return UnitVector(s1) - UnitVector(s2);
 }
 
 } // namespace astro
