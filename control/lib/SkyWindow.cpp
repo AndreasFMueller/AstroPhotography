37d155fd3e3c7a2b62e895e2ef75d07b34f1d5cd
Andreas Müller <andreas.mueller@othello.ch>
2014-08-08 22:46:36 +0200

    separate SkyWindow implementation

---
+++ control/lib/SkyWindow.cpp
@@ -0,0 +1,101 @@
+/*
+ * SkyWindow.cpp -- SkyWindow implementation
+ *
+ * (c) 2014 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#include <AstroCatalog.h>
+#include <AstroFormat.h>
+#include <AstroDebug.h>
+#include <includes.h>
+#include <CatalogBackend.h>
+
+namespace astro {
+namespace catalog {
+
+SkyWindow::SkyWindow(const RaDec& center,
+	const Angle& rawidth, const Angle& decheight) : _center(center) {
+	if (rawidth.radians() >= 2 * M_PI) {
+		_rawidth.radians(2 * M_PI);
+	} else {
+		_rawidth = rawidth;
+	}
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "window height: %f",
+		decheight.degrees());
+	_decheight = decheight.reduced(-M_PI / 2);
+}
+
+static double	reduce(double x, double left) {
+	return x - 2 * M_PI * floor((x - left) / (2 * M_PI));
+}
+
+std::string	SkyWindow::toString() const {
+	return stringprintf("%.3fx%.3f@%s", _rawidth.hours(),
+		_decheight.degrees(), _center.toString().c_str());
+}
+
+/**
+ * \brief find out whether a position is within the window
+ */
+bool	SkyWindow::contains(const RaDec& position) const {
+	// check right ascension
+	if (_rawidth.radians() < (2 * M_PI - 0.000000001)) {
+		double	left = _center.ra().radians() - _rawidth.radians() / 2.;
+		double	right = _center.ra().radians() + _rawidth.radians() / 2.;
+		double	ra = reduce(position.ra().radians(), left);
+		if (ra > right) {
+			return false;
+		}
+	}
+
+	// check declination
+	double	bottom = _center.dec().radians() - _decheight.radians() / 2.;
+	double	top = _center.dec().radians() + _decheight.radians() / 2.;
+	double	dec = reduce(position.dec().radians(), bottom);
+	//debug(LOG_DEBUG, DEBUG_LOG, 0, "top = %f, bottom = %f, dec = %f",
+	//	180 * top / M_PI, 180 * bottom / M_PI, 180 * dec / M_PI);
+	if (dec > top) {
+		return false;
+	}
+
+	// position is inside the window
+	return true;
+}
+
+std::pair<double, double>	SkyWindow::decinterval() const {
+	std::pair<double, double>	result;
+	result.first = _center.dec().radians() - _decheight.radians();
+	if (result.first < -M_PI/2) {
+		result.first = -M_PI/2;
+	}
+	result.second = _center.dec().radians() + _decheight.radians();
+	if (result.second > M_PI/2) {
+		result.second = M_PI/2;
+	}
+	return result;
+}
+
+Angle	SkyWindow::leftra() const {
+	return Angle(_center.ra() - _rawidth * 0.5).reduced();
+}
+
+Angle	SkyWindow::rightra() const {
+	return Angle(_center.ra() + _rawidth * 0.5).reduced();
+}
+
+Angle	SkyWindow::bottomdec() const {
+	return Angle(_center.dec() - _decheight * 0.5).reduced(-M_PI / 2);
+}
+
+SkyWindow	SkyWindow::all(RaDec(M_PI, 0), Angle(2 * M_PI), Angle(M_PI));
+
+void	SkyWindow::addMetadata(ImageBase& image) const {
+	image.setMetadata(std::string("RACENTR"), 
+		Metavalue(center().ra().hours(),
+			std::string("right ascension of image center in hours")));
+	image.setMetadata(std::string("DECCENTR"), 
+		Metavalue(center().dec().degrees(),
+			std::string("declination of image center in degrees")));
+}
+
+} // namespace catalog
+} // namespace astro
