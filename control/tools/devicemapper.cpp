fe54c46a56a5dff9539fafc1a20cffdec1a7783a
Andreas Mueller <afm@othello.ch>
2014-09-12 20:28:12 +0200

    devicemapper tool completed

--- control/tools/devicemapper.cpp
+++ control/tools/devicemapper.cpp
@@ -12,10 +12,17 @@
 #include <AstroConfig.h>
 #include <AstroDebug.h>
 #include <AstroFormat.h>
+#include <AstroLoader.h>
+#include <AstroUtils.h>
 
 using namespace astro::config;
+using namespace astro::module;
+using namespace astro::device;
 
 namespace astro {
+
+std::string	servername;
+bool	verbose = false;
 
 /**
  * \brief list of device mappingslist of device mappings
@@ -23,22 +30,142 @@ namespace astro {
 int	list_cmd(const std::vector<std::string>& /* arguments */) {
 	DeviceMapperPtr	devicemapper = Configuration::get()->devicemapper();
 	std::list<DeviceMap>	devices = devicemapper->select();
-	std::cout << "name     unit devicename                       server";
+	std::cout << "name     devicename                       unit server";
+	if (verbose) {
+		std::cout << ", description";
+	}
 	std::cout << std::endl;
 	std::for_each(devices.begin(), devices.end(),
 		[](const DeviceMap& devicemap) {
 			std::cout << stringprintf("%-8.8s ",
 				devicemap.name().c_str());
-			std::cout << stringprintf("%4d ", devicemap.unitid());
 			std::cout << stringprintf("%-32.32s ",
 				devicemap.devicename().toString().c_str());
-			std::cout << devicemap.servername();
+			std::cout << stringprintf("%4d ", devicemap.unitid());
+			if(devicemap.servername().size() == 0) {
+				std::cout << "(local)";
+			} else {
+				std::cout << devicemap.servername();
+			}
+			if (verbose) {
+				std::cout << ", " << devicemap.description();
+			}
 			std::cout << std::endl;
 		}
 	);
 	return EXIT_SUCCESS;
 }
 
+/**
+ * \brief Auxiliary functor class to scan devices for a single driver module
+ */
+class scan_module {
+public:
+	void	operator()(const std::string& modulename);
+};
+
+/**
+ * \brief Display the contens of a named module
+ */
+void	scan_module::operator()(const std::string& modulename) {
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "scanning module '%s'",
+		modulename.c_str());
+	if (servername.size() != 0) {
+		std::cerr << "remove device mapping currently not supported";
+		std::cerr << std::endl;
+		throw std::runtime_error("remote device mapping not supported");
+	}
+
+	// get a repository
+	Repository	repository;
+
+	// get the module
+	ModulePtr	module = repository.getModule(modulename);
+
+	// read the device locator from a module
+	DeviceLocatorPtr	locator = module->getDeviceLocator();
+
+	// get a list of devices
+	std::vector<std::string>	devices = locator->getDevicelist();
+
+	// display the devices
+	std::for_each(devices.begin(), devices.end(), 
+		[](const std::string& devicename) {
+			std::cout << devicename << std::endl;
+		}
+	);
+}
+
+/**
+ * \brief Scan the devices offered by modules defined in the arguments
+ */
+int	scan_cmd(const std::vector<std::string>& arguments) {
+	if (arguments.size() < 2) {
+		throw std::runtime_error("scan command needs at least one "
+			"module name argument");
+	}
+	std::for_each(arguments.begin() + 1, arguments.end(), scan_module());
+	return EXIT_SUCCESS;
+}
+
+/**
+ * \brief create or update a map
+ */
+int	map_cmd(const std::vector<std::string> arguments) {
+	// check that we have at least the map argument
+	if (arguments.size() < 3) {
+		throw std::runtime_error("at least map name and device name required");
+	}
+	std::string	mapname = arguments[1];
+	DeviceName	devname = DeviceName(arguments[2]);
+
+	// get the current mapping entry
+	ConfigurationPtr	config = Configuration::get();
+	DeviceMapperPtr	devicemapper = config->devicemapper();
+	DeviceMap	devmap(devname);
+	devmap.name(mapname);
+	bool	newmapping = false;
+	try {
+		devmap = devicemapper->find(mapname);
+	} catch (...) {
+		newmapping = true;
+	}
+
+	// set other values from further arguments
+	AttributeValuePairs	pairs(arguments, 3);
+	if (pairs.has("description")) {
+		devmap.description(pairs("description"));
+	}
+	if (pairs.has("unit")) {
+		devmap.unitid(std::stol(pairs("unit")));
+	}
+	devmap.servername(servername);
+
+	// perform the update
+	if (newmapping) {
+		devicemapper->add(devmap);
+	} else {
+		devicemapper->update(mapname, devmap);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+/**
+ * \brief remove a mapping from the device
+ */
+int	remove_cmd(const std::vector<std::string>& arguments) {
+	if (arguments.size() < 2) {
+		throw std::runtime_error("missing map name");
+	}
+	std::string	mapname = arguments[1];
+	Configuration::get()->devicemapper()->remove(mapname);
+	return EXIT_SUCCESS;
+}
+
+/**
+ * \brief Display commands 
+ */
 void	usage(const std::string& progname) {
 	std::cerr << "usage:" << std::endl;
 	std::cerr << progname << " [ options ] list" << std::endl;
@@ -50,6 +177,7 @@ static struct option	longopts[] = {
 { "debug",	no_argument,		NULL,		'd' }, /* 1 */
 { "help",	no_argument,		NULL,		'h' }, /* 2 */
 { "server",	required_argument,	NULL,		's' }, /* 3 */
+{ "verbose",	no_argument,		NULL,		'v' }, /* 4 */
 { NULL,		0,			NULL,		0   }
 };
 
@@ -57,10 +185,9 @@ static struct option	longopts[] = {
  * \brief The main method of the devicemapper command
  */
 int	main(int argc, char *argv[]) {
-	std::string	servername;
 	int	c;
 	int	longindex;
-	while (EOF != (c = getopt_long(argc, argv, "c:dhs:", longopts,
+	while (EOF != (c = getopt_long(argc, argv, "c:dhs:v", longopts,
 		&longindex))) {
 		switch (c) {
 		case 'c':
@@ -74,6 +201,9 @@ int	main(int argc, char *argv[]) {
 			break;
 		case 's':
 			servername = optarg;
+			break;
+		case 'v':
+			verbose = true;
 			break;
 		case 1:
 			switch (longindex) {
@@ -95,6 +225,15 @@ int	main(int argc, char *argv[]) {
 	std::string	cmd = arguments[0];
 	if (cmd == "list") {
 		return list_cmd(arguments);
+	}
+	if (cmd == "scan") {
+		return scan_cmd(arguments);
+	}
+	if (cmd == "map") {
+		return map_cmd(arguments);
+	}
+	if (cmd == "remove") {
+		return remove_cmd(arguments);
 	}
 
 	// 
