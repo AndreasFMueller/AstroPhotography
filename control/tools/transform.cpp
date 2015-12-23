34839822d03e2aaea2bb26e5f1b5d156e836123f
Andreas Mueller <afm@othello.ch>
2015-12-23 13:37:05 +0100

    add long options/usage to transform program

--- control/tools/transform.cpp
+++ control/tools/transform.cpp
@@ -23,13 +23,52 @@ namespace astro {
 namespace app {
 namespace transform {
 
+static void	usage(const char *progname) {
+	Path	p(progname);
+	std::cout << "usage:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "    " << p.basename() << " [ options ] <in.fits> "
+		"<out.fits>" << std::endl;
+	std::cout << std::endl;
+	std::cout << "read an image from <in.fits>, translate or rotate it, "
+		"and write the result" << std::endl;
+	std::cout << "to <out.fits>." << std::endl;
+	std::cout << std::endl;
+	std::cout << "options:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "    -d,--debug          increase debug level"
+		<< std::endl;
+	std::cout << "    -a,-angle=<angle>   rotate through angle <angle>"
+		<< std::endl;
+	std::cout << "    -x,--x-offset=<x>   translate <x> in x-direction"
+		<< std::endl;
+	std::cout << "    -y,--y-offset=<y>   translate <y> in y-direction"
+		<< std::endl;
+	std::cout << "    -s,--sample=<value> down or upsample the image"
+		<< std::endl;
+	std::cout << "    -h,-?,--help        display this help message and "
+		"exit" << std::endl;
+	std::cout << std::endl;
+}
+
+static struct option	longopts[] = {
+{ "debug",	no_argument,		NULL,	'd' }, /* 0 */
+{ "angle",	required_argument,	NULL,	'a' }, /* 0 */
+{ "x-offset",	required_argument,	NULL,	'x' }, /* 0 */
+{ "y-offset",	required_argument,	NULL,	'y' }, /* 0 */
+{ "sample",	required_argument,	NULL,	's' }, /* 0 */
+{ "help",	no_argument,		NULL,	'h' }, /* 0 */
+};
+
 int	main(int argc, char *argv[]) {
 	// parse command line
 	int	c;
+	int	longindex;
 	Point	translation;
 	int	sample = 0;
 	double	angle = 0;
-	while (EOF != (c = getopt(argc, argv, "dx:y:s:a:")))
+	while (EOF != (c = getopt_long(argc, argv, "dx:y:s:a:h?",
+		longopts, &longindex)))
 		switch (c) {
 		case 'd':
 			debuglevel = LOG_DEBUG;
@@ -46,6 +85,10 @@ int	main(int argc, char *argv[]) {
 		case 's':
 			sample = atoi(optarg);
 			break;
+		case 'h':
+		case '?':
+			usage(argv[0]);
+			return EXIT_SUCCESS;
 		}
 
 	// next two arguments must be filenames
