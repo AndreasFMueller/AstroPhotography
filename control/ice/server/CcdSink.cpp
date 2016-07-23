3b59cc227808627d1b78c8047f2441f25a41d840
Andreas Mueller <afm@othello.ch>
2016-07-23 09:56:26 +0200

    improve CcdSink implementation

--- control/ice/server/CcdSink.cpp
+++ control/ice/server/CcdSink.cpp
@@ -1,5 +1,5 @@
 /*
- * CcdSink.cpp
+ * CcdSink.cpp -- Implementation of the CcdSink class
  *
  * (c) 2016 Prof Dr Andreas Mueller, Hochschule Rapperswil
  */
@@ -9,6 +9,12 @@
 
 namespace snowstar {
 
+/**
+ * \brief constructor for the CcdSink
+ *
+ * The constructor creates the ImageSink proxy via which it will talk
+ * to the client
+ */
 CcdSink::CcdSink(const Ice::Identity& identity, const Ice::Current& current) {
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "construct a CcdSink");
 	Ice::ObjectPrx	oneway = current.con->createProxy(identity)
@@ -16,13 +22,32 @@ CcdSink::CcdSink(const Ice::Identity& identity, const Ice::Current& current) {
 	sinkprx = ImageSinkPrx::uncheckedCast(oneway);
 }
 
+/**
+ * \brief Image sink main method
+ *
+ * The operator() implementation absorbs ImageQueueEntries from the camera
+ * converts them to ImageQueueEntries for the ICE protocol and sends them
+ * to the client.
+ */
 void	CcdSink::operator()(const astro::camera::ImageQueueEntry& entry) {
-	ImageQueueEntryPtr	e = convert(entry);
-	sinkprx->image(*e);
+	// don't do anything if we have no proxy (this should not happen,
+	// we play safe here)
+	if (sinkprx) {
+		ImageQueueEntryPtr	e = convert(entry);
+		sinkprx->image(*e);
+	}
 }
 
+/**
+ * \brief stop operation
+ *
+ * The stop operation tells the client that no more images will be forthcoming
+ * and that it can remove the adapter
+ */
 void	CcdSink::stop() {
-	sinkprx->stop();
+	if (sinkprx) {
+		sinkprx->stop();
+	}
 }
 
 } // namespace snowstar
