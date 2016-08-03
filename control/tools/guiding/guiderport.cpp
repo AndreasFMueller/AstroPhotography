694cd394ddbf1468a0b0d1dd64b06fac34140b34
Andreas Mueller <afm@othello.ch>
2016-08-03 16:32:45 +0200

    improve guiderport utility usability

--- control/tools/guiding/guiderport.cpp
+++ control/tools/guiding/guiderport.cpp
@@ -27,10 +27,10 @@ static void	usage(const char *progname) {
 	std::cout << "      RA+, RA-, DEC+, DEC-" << std::endl;
 	std::cout << "   2: do binary count using the port bits in increased significance" << std::endl;
 	std::cout << "      as RA+, RA-, DEC+, DEC-" << std::endl;
+	std::cout << "   3: turn on all outputs, then turn of one by one every second" << std::endl;
+	std::cout << "   4: RA backlash calibration 1s RA+, 1s RA-" << std::endl;
+	std::cout << "   5: DEC backlash calibration 1s DEC+, 1s DEC-" << std::endl;
 	std::cout << "options:" << std::endl;
-	std::cout << "   3: " << std::endl;
-	std::cout << "   4: RA backlash calibration 3s RA+, 3s RA-" << std::endl;
-	std::cout << "   5: DEC backlash calibration 3s DEC+, 3s DEC-" << std::endl;
 	std::cout << "  -d,--debug      increase debug level" << std::endl;
 	std::cout << "  -h,--help       display this help message and exit";
 	std::cout << "  -s,--scale=s    scale all times by the factor s";
@@ -46,17 +46,41 @@ static struct option	longopts[] = {
 
 static float scale = 1;
 
+#define	RP	GuiderPort::RAPLUS
+#define	RM	GuiderPort::RAMINUS
+#define	DP	GuiderPort::DECPLUS
+#define	DM	GuiderPort::DECMINUS
+
+Timer	globaltimer;
+
+void	display_header() {
+	printf("time:  RA+  RA- DEC+ DEC-\n");
+}
+
+void	display(astro::camera::GuiderPortPtr guiderport,
+		double raplus, double raminus,
+		double decplus, double decminus) {
+	globaltimer.end();
+	printf("%4.1f:", globaltimer.elapsed());
+	if (raplus > 0) { printf("%5.1f", raplus); } else { printf(" ____"); }
+	if (raminus > 0) { printf("%5.1f", raminus); } else { printf(" ____"); }
+	if (decplus > 0) { printf("%5.1f", decplus); } else { printf(" ____"); }
+	if (decminus > 0) { printf("%5.1f", decminus); } else { printf(" ____"); }
+	printf("\n");
+	guiderport->activate(raplus, raminus, decplus, decminus);
+}
+
 void	prog0(astro::camera::GuiderPortPtr guiderport) {
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "starting program 1");
 	int	pause = scale + 2;
 	while (1) {
-		guiderport->activate(scale, 0, 0, 0);
+		display(guiderport, scale, 0, 0, 0);
 		sleep(pause);
-		guiderport->activate(0, scale, 0, 0);
+		display(guiderport, 0, scale, 0, 0);
 		sleep(pause);
-		guiderport->activate(0, 0, scale, 0);
+		display(guiderport, 0, 0, scale, 0);
 		sleep(pause);
-		guiderport->activate(0, 0, 0, scale);
+		display(guiderport, 0, 0, 0, scale);
 		sleep(pause);
 	}
 }
@@ -70,7 +94,7 @@ void	prog1(astro::camera::GuiderPortPtr guiderport) {
 			float	raminus = (i & 2) ? scale : 0;
 			float	decplus = (i & 4) ? scale : 0;
 			float	decminus = (i & 8) ? scale : 0;
-			guiderport->activate(raplus, raminus,
+			display(guiderport, raplus, raminus,
 				decplus, decminus);
 			sleep(pause);
 		}
@@ -82,13 +106,13 @@ void	prog2(astro::camera::GuiderPortPtr guiderport) {
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "starting program 3");
 	int	pause = 5 * scale;
 	while (1) {
-		guiderport->activate(4 * scale, 3 * scale, 2 * scale, 1 * scale);
+		display(guiderport, 4 * scale, 3 * scale, 2 * scale, 1 * scale);
 		sleep(pause);
-		guiderport->activate(3 * scale, 2 * scale, 1 * scale, 4 * scale);
+		display(guiderport, 3 * scale, 2 * scale, 1 * scale, 4 * scale);
 		sleep(pause);
-		guiderport->activate(2 * scale, 1 * scale, 4 * scale, 3 * scale);
+		display(guiderport, 2 * scale, 1 * scale, 4 * scale, 3 * scale);
 		sleep(pause);
-		guiderport->activate(1 * scale, 4 * scale, 3 * scale, 2 * scale);
+		display(guiderport, 1 * scale, 4 * scale, 3 * scale, 2 * scale);
 		sleep(pause);
 	}
 }
@@ -100,9 +124,9 @@ void	prog3(astro::camera::GuiderPortPtr guiderport) {
 		pause = 1;
 	}
 	while (1) {
-		guiderport->activate(pause,0,0,0);
+		display(guiderport, pause,0,0,0);
 		sleep(pause);
-		guiderport->activate(0,pause,0,0);
+		display(guiderport, 0,pause,0,0);
 		sleep(pause);
 	}
 }
@@ -114,9 +138,9 @@ void	prog4(astro::camera::GuiderPortPtr guiderport) {
 		pause = 1;
 	}
 	while (1) {
-		guiderport->activate(0,0,pause,0);
+		display(guiderport, 0, 0, pause, 0);
 		sleep(pause);
-		guiderport->activate(0,0,0,pause);
+		display(guiderport, 0, 0, 0, pause);
 		sleep(pause);
 	}
 }
@@ -173,6 +197,8 @@ int	main(int argc, char *argv[]) {
 	if ((prognumber > 4) || (prognumber < 0)) {
 		throw std::runtime_error("unknown program number");
 	}
+	globaltimer.start();
+	display_header();
 	programtable[prognumber](guiderport);
 
 	return EXIT_SUCCESS;
