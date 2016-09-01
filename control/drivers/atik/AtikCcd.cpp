63bdcce4550d939e0dd1d82135e25cb9fa8f49ad
Andreas Müller <andreas.mueller@othello.ch>
2016-09-01 21:53:46 +0200

    upgrade to new Atik SDK 1.5

--- control/drivers/atik/AtikCcd.cpp
+++ control/drivers/atik/AtikCcd.cpp
@@ -13,30 +13,52 @@ namespace astro {
 namespace camera {
 namespace atik {
 
+/**
+ * \brief Construct the CCD for an Atik-Camera
+ */
 AtikCcd::AtikCcd(CcdInfo& info, ::AtikCamera *camera)
 	: Ccd(info), _camera(camera) {
         // get the capabilities
 	const char	*name;
 	CAMERA_TYPE	type;
 	_camera->getCapabilities(&name, &type, &capa);
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "created AtikCcd %s, %s", name,
+		(type == ORIGINAL_HSC) ? "HSC" : (
+		(type == IC24) ? "IC24" : (
+		(type == QUICKER) ? "QUICKER" : (
+		(type == IIDC) ? "SONY_SCI" : "(unknown)"))));
 }
 
+/** 
+ * \brief destroy the atik camera
+ */
 AtikCcd::~AtikCcd() {
 }
 
+/**
+ * \brief Private run method that performs the actual exposure on an ATIK camera
+ */
 void	AtikCcd::run() {
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "run method of AtikCcd starting");
 	_image = ImagePtr(NULL);
 	bool	rc;
+
+	// set the shutter if we have
 	if (capa.hasShutter) {
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "setting shutter to %s",
 			(exposure.needsshutteropen()) ? "open" : "closed");
 		rc = _camera->setShutter(exposure.needsshutteropen());
+		if (!rc) {
+			std::string	msg = stringprintf("cannot set shutter:"
+				" %s", _camera->getLastError());
+			debug(LOG_ERR, DEBUG_LOG, 0, "%s");
+			throw std::runtime_error(msg);
+		}
 	}
-	rc = _camera->startExposure(true);
-	if (!rc) {
-		// OK, we assume that we cannot to a long exposure, try a
-		// short one
+
+	// now we have to decide whether we can do a long exposure
+	if (exposure.exposuretime() < capa.maxShortExposure) {
+		// do a short exposure
 		rc = _camera->readCCD(exposure.x(), exposure.y(),
 			exposure.width(), exposure.height(),
 			exposure.mode().x(), exposure.mode().y(),
@@ -44,11 +66,29 @@ void	AtikCcd::run() {
 		if (!rc) {
 			state(CcdState::exposed);
 			std::string	msg = stringprintf("cannot do "
-				"exposure %.3f sec", exposure.exposuretime());
+				"exposure %.3f sec: %s",
+				exposure.exposuretime(),
+				_camera->getLastError());
 			debug(LOG_ERR, DEBUG_LOG, 0, "%s", msg.c_str());
 			throw std::runtime_error(msg);
 		}
 	} else {
+		if (!capa.supportsLongExposure) {
+			std::string	msg = stringprintf("camera does not "
+				"support long exposure, and exposure time %.3f "
+				"exceeds limit %.3f for short exposures",
+				exposure.exposuretime(), capa.maxShortExposure);
+			debug(LOG_ERR, DEBUG_LOG, 0, "%s", msg.c_str());
+			throw std::runtime_error(msg);
+		}
+		rc = _camera->startExposure(true);
+		if (!rc) {
+			// cannot start exposure
+			std::string	msg = stringprintf("cannot start long "
+				"exposure: %s", _camera->getLastError());
+			debug(LOG_ERR, DEBUG_LOG, 0, "%s", msg.c_str());
+			throw std::runtime_error(msg);
+		}
 		// wait for the exposure time to expire
 		usleep(_camera->delay(exposure.exposuretime()));
 		rc = _camera->readCCD(exposure.x(), exposure.y(),
@@ -56,11 +96,14 @@ void	AtikCcd::run() {
 			exposure.mode().x(), exposure.mode().y());
 		if (!rc) {
 			state(CcdState::exposed);
-			std::string	msg = stringprintf("cannot read after long exposure");
+			std::string	msg = stringprintf("cannot read after "
+				"long exposure: %s", _camera->getLastError());
 			debug(LOG_ERR, DEBUG_LOG, 0, "%s", msg.c_str());
 			throw std::runtime_error(msg);
 		}
 	}
+
+	// interpreting the data we have received
 	debug(LOG_DEBUG, DEBUG_LOG, 0, "exposure %s complete. reading rata",
 		exposure.toString().c_str());
 	ImageSize	size = exposure.frame().size() / exposure.mode();
@@ -71,7 +114,8 @@ void	AtikCcd::run() {
 	rc = _camera->getImage(image->pixels, image->getSize().getPixels());
 	if (!rc) {
 		state(CcdState::exposed);
-		std::string	msg = stringprintf("cannot read data");
+		std::string	msg = stringprintf("cannot read data: %s",
+			_camera->getLastError());
 		debug(LOG_ERR, DEBUG_LOG, 0, "%s", msg.c_str());
 		throw std::runtime_error(msg);
 	}
