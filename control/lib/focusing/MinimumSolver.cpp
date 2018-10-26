7e15e9dda7667174edea08e0c454b02e6110711f
Andreas Müller <andreas.mueller@hsr.ch>
2018-10-26 19:23:23 +0200

    add missing class MinimumSolver

---
+++ control/lib/focusing/MinimumSolver.cpp
@@ -0,0 +1,40 @@
+/*
+ * MinimumSolver.cpp -- solves by looking for the minimum
+ *
+ * (c) 2016 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#include <AstroFocus.h>
+#include "FocusSolvers.h"
+
+namespace astro {
+namespace focusing {
+
+MinimumSolver::MinimumSolver() {
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "create a new MinimumSolver");
+}
+
+int	MinimumSolver::position(const FocusItems& focusitems) {
+	FocusItems::const_iterator	i;
+	minimumposition = -1;
+	minimum = 0;
+	minimum = std::numeric_limits<float>::max();
+	for (i = focusitems.begin(); i != focusitems.end(); i++) {
+		float	value = i->value();
+		if (value < minimum) {
+			minimumposition = i->position();
+			minimum = i->value();
+		}
+		if (value > minimum) {
+			minimum = value;
+		}
+	}
+	if (minimumposition < 0) {
+		std::string	msg("minimum not found, not solvable");
+		debug(LOG_ERR, DEBUG_LOG, 0, "%s", msg.c_str());
+		throw std::string(msg);
+	}
+	return minimumposition;
+}
+
+} // namespace focusing
+} // namespace astro
