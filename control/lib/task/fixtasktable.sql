feb594c22972ea9c2e93b151eaedc00c60ca1ddf
Andreas Müller <andreas.mueller@othello.ch>
2019-07-14 14:14:58 +0200

    fix typos in database migration script

--- control/lib/task/fixtasktable.sql
+++ control/lib/task/fixtasktable.sql
@@ -3,10 +3,10 @@
 --
 -- (c) 2019 Prof Dr Andreas Müller, Hochschule Rapperswil
 --
-alter table taskqueue add column tasktype integer not null 0;
-alter table taskqueue add column guiderccdindex integer not null -1;
+alter table taskqueue add column tasktype integer not null default 0;
+alter table taskqueue add column guiderccdindex integer not null default -1;
 alter table taskqueue add column guiderccd varchar(256) not null default '';
-alter table taskqueue add column guideportindex integer not null -1;
-alter table taskqueue add column guideportccd varchar(256) not null default '';
-alter table taskqueue add column adaptiveopticsindex integer not null -1;
+alter table taskqueue add column guideportindex integer not null default -1;
+alter table taskqueue add column guideport varchar(256) not null default '';
+alter table taskqueue add column adaptiveopticsindex integer not null default -1;
 alter table taskqueue add column adaptiveopticsccd varchar(256) not null default '';
