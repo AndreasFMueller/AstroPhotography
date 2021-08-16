eaf53a07adcb10ec0ae94d3890defea6e149d32b
Andreas Müller <andreas.mueller@othello.ch>
2021-08-16 10:11:10 +0200

    fix file backend adding error messages for missing files

--- control/lib/catalogs/FileBackend.cpp
+++ control/lib/catalogs/FileBackend.cpp
@@ -25,20 +25,47 @@ const float	Tycho2_cutover_magnitude = 10;
  *
  * The constructor ensures that all the files or directories (in the case of
  * the Ucac4 catalog) are present
+ *
+ * \param basedir	the base directory where all the catalogs can be found
  */
 FileBackend::FileBackend(const std::string& basedir) : _basedir(basedir) {
 	backendname = stringprintf("Files(%s)", basedir.c_str());
 	// ensure that the files for the file backend exist
-	bsc_catalog = CatalogPtr(new BSC(_basedir
-					+ std::string("/bsc")));
-	hipparcos_catalog = CatalogPtr(new Hipparcos(_basedir
-					+ std::string("/hipparcos")));
-	sao_catalog = CatalogPtr(new SAO(_basedir
-					+ std::string("/sao")));
-	tycho2_catalog = CatalogPtr(new Tycho2(_basedir
-					+ std::string("/tycho2")));
-	ucac4_catalog = CatalogPtr(new Ucac4(_basedir
-					+ std::string("/u4")));
+	try {
+		bsc_catalog = CatalogPtr(new BSC(_basedir
+						+ std::string("/bsc")));
+	} catch (const std::exception& x) {
+		debug(LOG_ERR, DEBUG_LOG, 0, "BSC files not found: %s",
+			x.what());
+	}
+	try {
+		hipparcos_catalog = CatalogPtr(new Hipparcos(_basedir
+						+ std::string("/hipparcos")));
+	} catch (const std::exception& x) {
+		debug(LOG_ERR, DEBUG_LOG, 0, "Hipparcos files not found: %s",
+			x.what());
+	}
+	try {
+		sao_catalog = CatalogPtr(new SAO(_basedir
+						+ std::string("/sao")));
+	} catch (const std::exception& x) {
+		debug(LOG_ERR, DEBUG_LOG, 0, "SAO files not found: %s",
+			x.what());
+	}
+	try {
+		tycho2_catalog = CatalogPtr(new Tycho2(_basedir
+						+ std::string("/tycho2")));
+	} catch (const std::exception& x) {
+		debug(LOG_ERR, DEBUG_LOG, 0, "Tycho2 files not found: %s",
+			x.what());
+	}
+	try {
+		ucac4_catalog = CatalogPtr(new Ucac4(_basedir
+						+ std::string("/u4")));
+	} catch (const std::exception& x) {
+		debug(LOG_ERR, DEBUG_LOG, 0, "U4 files not found: %s",
+			x.what());
+	}
 }
 
 /**
@@ -66,7 +93,7 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 
 	// get the brightest stars from the BSC catalog (because the
 	// brightest stars are not in the Hipparcos catalog)
-	if (magrange.brightest() <= BSC_cutover_magnitude) {
+	if (bsc_catalog && (magrange.brightest() <= BSC_cutover_magnitude)) {
 		// construct magnitude range to retrieve from BSC
 		MagnitudeRange	bsc_range(-30,
 			(magrange.faintest() > BSC_cutover_magnitude) ?
@@ -80,10 +107,12 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 		}
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "%d stars from BSC added, "
 			"now %d stars", stars->size(), result->size());
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "BSC catalog missing");
 	}
 
 	// if any there are stars requested from the Hipparcos catalog, get them
-	if (magrange.brightest() <= Hipparcos_cutover_magnitude) {
+	if (hipparcos_catalog && (magrange.brightest() <= Hipparcos_cutover_magnitude)) {
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "adding stars from Hipparcos");
 		// get brightest stars from Hipparcos catalog
 		starsetptr	stars
@@ -95,6 +124,8 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 		}
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "%d stars from Hipparcos added, "
 			"now %d stars", stars->size(), result->size());
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "Hipparcos catalog missing");
 	}
 
 	// if the faintest magnitude is brighter than the magnitude to
@@ -108,7 +139,7 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 
 	// get the intermediate stars from the Tycho2 catalog, but skip the
 	// stars already retrieved from the Hipparcos catalog
-	if (magrange.brightest() <= Tycho2_cutover_magnitude) {
+	if (tycho2_catalog && (magrange.brightest() <= Tycho2_cutover_magnitude)) {
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "adding stars from Tycho2");
 		starsetptr	stars
 			= tycho2_catalog->find(window, magrange);
@@ -124,6 +155,8 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 		}
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "%d stars from Tycho2 added, "
 			"now %d stars", stars->size(), result->size());
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "Tycho2 catalog missing");
 	}
 
 	// if the faintestmagnitude is bright enough so that the Tycho-2
@@ -135,7 +168,7 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 	}
 
 	// get all matching stars from the UCAC4 catalog
-	{
+	if (ucac4_catalog) {
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "adding stars from UCAC4");
 		starsetptr	stars
 			= ucac4_catalog->find(window, magrange);
@@ -148,6 +181,8 @@ Catalog::starsetptr	FileBackend::find(const SkyWindow& window,
 		}
 		debug(LOG_DEBUG, DEBUG_LOG, 0, "%d stars from UCAC4 added, "
 			"now %d stars", stars->size(), result->size());
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "ucac4 catalog missing");
 	}
 
 	return resultptr;
@@ -163,47 +198,74 @@ CatalogIterator	FileBackend::findIter(const SkyWindow& window,
 
 /**
  * \brief Get a star from the unified catalogs by name
+ *
+ * Retrieve a particular star by name.
+ *
+ * \param name	the name of the star
  */
 Star	FileBackend::find(const std::string& name) {
 	// check for BSC stars
-	if (name.substr(0, 3) == "BSC") {
+	if ((name.substr(0, 3) == "BSC") && bsc_catalog) {
 		Star	star = bsc_catalog->find(name);
 		return star;
 	}
 
 	// check for Hipparcos stars
-	if (name.substr(0, 3) == "HIP") {
+	if ((name.substr(0, 3) == "HIP") && hipparcos_catalog) {
 		Star	star = hipparcos_catalog->find(name);
 		return star;
 	}
 
 	// check for Hipparcos stars
-	if (name.substr(0, 3) == "SAO") {
+	if ((name.substr(0, 3) == "SAO") && sao_catalog) {
 		Star	star = sao_catalog->find(name);
 		return star;
 	}
 
 	// check for Tycho2 star
-	if (name.substr(0, 1) == "T") {
+	if ((name.substr(0, 1) == "T") && tycho2_catalog) {
 		Star	star = tycho2_catalog->find(name);
 		return star;
 	}
 
 	// check for Ucac4 star
-	if (name.substr(0, 5) == "UCAC4") {
+	if ((name.substr(0, 5) == "UCAC4") && ucac4_catalog) {
 		Star	star = ucac4_catalog->find(name);
 		return star;
 	}
 	throw std::runtime_error("unkonwn star name");
 }
 
+/**
+ * \grief Retrieve the number of stars in the catalog
+ */
 unsigned long	FileBackend::numberOfStars() {
 	unsigned long	result = 0;
-	result += bsc_catalog->numberOfStars();
-	result += hipparcos_catalog->numberOfStars();
-	result += sao_catalog->numberOfStars();
-	result += tycho2_catalog->numberOfStars();
-	result += ucac4_catalog->numberOfStars();
+	if (bsc_catalog) {
+		result += bsc_catalog->numberOfStars();
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "BSC catalog missing");
+	}
+	if (hipparcos_catalog) {
+		result += hipparcos_catalog->numberOfStars();
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "Hipparcos catalog missing");
+	}
+	if (sao_catalog) {
+		result += sao_catalog->numberOfStars();
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "SAO catalog missing");
+	}
+	if (tycho2_catalog) {
+		result += tycho2_catalog->numberOfStars();
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "Tycho2 catalog missing");
+	}
+	if (ucac4_catalog) {
+		result += ucac4_catalog->numberOfStars();
+	} else {
+		debug(LOG_ERR, DEBUG_LOG, 0, "UCAC4 catalog missing");
+	}
 	return result;
 }
 
