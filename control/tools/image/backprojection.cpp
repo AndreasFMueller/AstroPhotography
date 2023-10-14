d512f00be4effac878ab8788ee940d2a3f5657f1
Andreas Müller <andreas.mueller@ost.ch>
2023-10-14 20:01:46 +0200

    more improvements to backprojection program

--- control/tools/image/backprojection.cpp
+++ control/tools/image/backprojection.cpp
@@ -9,6 +9,7 @@
 #include <AstroIO.h>
 #include <Radon.h>
 #include <AstroAdapter.h>
+#include <AstroConvolve.h>
 
 using namespace astro;
 using namespace astro::io;
@@ -34,13 +35,14 @@ static void	usage(const char *progname) {
 	std::cout << std::endl;
 	std::cout << "options:" << std::endl;
 	std::cout << std::endl;
-	std::cout << "    -d,--debug                "
+	std::cout << "    -d,--debug              "
 		"increase debug level" << std::endl;
-	std::cout << "    -h,--height=<height>      "
+	std::cout << "    -h,--height=<height>    "
 		"divide 180 degrees in <height> steps" << std::endl;
 	std::cout << "    -w,--width=<width>      width of the backprojection transform image"
 		<< std::endl;
-	std::cout << "    -h,-?,--help              "
+	std::cout << "    -f,--filter             also filter the backprojekction" << std::endl;
+	std::cout << "    -h,-?,--help            "
 		"show this help message" << std::endl;
 	std::cout << std::endl;
 }
@@ -49,7 +51,8 @@ static struct option	longopts[] = {
 { "width",		required_argument,	NULL,	'w' }, /* 0 */
 { "height",		required_argument,	NULL,	'h' }, /* 1 */
 { "debug",		no_argument,		NULL,	'd' }, /* 2 */
-{ "help",		no_argument,		NULL,	'?' }, /* 2 */
+{ "filter",		no_argument,		NULL,	'f' }, /* 3 */
+{ "help",		no_argument,		NULL,	'?' }, /* 4 */
 { NULL,			0,			NULL,	0   }
 };
 
@@ -60,11 +63,12 @@ static struct option	longopts[] = {
  * from them.
  */
 int	main(int argc, char *argv[]) {
-	int	width = 512;
-	int	height = 512;
+	int	width = -1;
+	int	height = -1;
 	int	c;
 	int	longindex;
-	while (EOF != (c = getopt_long(argc, argv, "dw:h:?",
+	bool	filter = false;
+	while (EOF != (c = getopt_long(argc, argv, "dw:h:f?",
 		longopts, &longindex)))
 		switch (c) {
 		case 'd':
@@ -75,6 +79,9 @@ int	main(int argc, char *argv[]) {
 			break;
 		case 'w':
 			width = atoi(optarg);
+			break;
+		case 'f':
+			filter = true;
 			break;
 		case '?':
 			usage(argv[0]);
@@ -98,21 +105,69 @@ int	main(int argc, char *argv[]) {
 	ImagePtr	radonptr = in.read();
 	DoubleAdapter	radon(radonptr);
 	Image<double>	rawradon(radon);
-#if 0
-	FITSoutfile<double>	outimage("backprojectionimage.fits");
-	outimage.setPrecious(false);
-	outimage.write(rawimage);
-#endif
+
+	// if width or height are not set, we set them from the image
+	if (width < 0) {
+		width = radonptr->size().width();
+	}
+	if (height < 0) {
+		height = radonptr->size().height();
+	}
 
 	// perform the backprojection transform
 	ImageSize	backprojectionsize(width, height);
 	BackProjection	backprojection(backprojectionsize, rawradon);
 	Image<double>	backprojectionimage(backprojection);
 
-	// write the result
-	io::FITSoutfile<double>	out(outfile);
+	// if filtering is not required, return the image
+	if (!filter) {
+		io::FITSoutfile<double>	out(outfile);
+		out.setPrecious(false);
+		out.write(backprojectionimage);
+		return EXIT_SUCCESS;
+	}
+
+	// perform the filtering
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "filtering required");
+	FourierImage	fi(backprojectionimage);
+	FourierImage	filterimage(backprojectionimage.size());
+	int	w = filterimage.size().width();
+	int	W = w / 2;
+	int	h = filterimage.size().height();
+	int	rmax = ((W < h) ? W : h) / 2;
+	debug(LOG_DEBUG, DEBUG_LOG, 0,
+		"fourier image has size %d x %d, rmax = %d", w, h, rmax);
+	for (int x = 0; x < w / 2; x++) {
+		for (int y = 0; y < h; y++) {
+			double	r = 0;
+			double	r00 = hypot(x, y);
+			double	r01 = hypot(x, h - y);
+			//double	r10 = hypot(W - x, y);
+			//double	r11 = hypot(W - x, h - y);
+			if (r00 < rmax) {
+				r = r00;
+			}
+			if (r01 < rmax) {
+				r = r01;
+			}
+			//if (r10 < rmax) {
+			//	r = r10;
+			//}
+			//if (r11 < rmax) {
+			//	r = r11;
+			//}
+			filterimage.pixel(2 * x, y) = r;
+			filterimage.pixel(2 * x + 1, y) = 0;
+		}
+	}
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "filter function initialized");
+	FourierImagePtr	fourierfiltered = fi * filterimage;
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "fourier function multiplied");
+	ImagePtr	filtered = fourierfiltered->inverse();
+	debug(LOG_DEBUG, DEBUG_LOG, 0, "multiplied");
+	io::FITSout	out(outfile);
 	out.setPrecious(false);
-	out.write(backprojectionimage);
+	out.write(filtered);
 
 	return EXIT_SUCCESS;
 }
